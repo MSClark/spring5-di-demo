1dca7e9d65e2fec7372a7bbb0cfd604aca46bb92
msclark <msclark3@asu.edu>
2019-10-31 15:13:26 -0700

    testing datasource and properties files

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,35 @@
+package guru.springframework.config;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties") //brings in datasource properties
+public class PropertyConfig {
+
+    @Value("${clark.username}") //spring expression language gets the properties out of the datasource.properties file
+    String user;
+    @Value("$(clark.password}")
+    String password;
+    @Value("${clark.dburl}")
+    String dbUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setDbUrl(dbUrl);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
